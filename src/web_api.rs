2792c8fee453aedfbc3145395c8fb4f2cb8e5f4a
Michael <3534322+gewmas@users.noreply.github.com>
2026-05-24 17:32:59 -0700

    POST /users

--- src/web_api.rs
+++ src/web_api.rs
@@ -1,11 +1,26 @@
-use axum::{routing::get, Json, Router};
-use serde::Serialize;
+use axum::{
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 
 #[derive(Serialize)]
 struct StatusResponse {
     status: String,
     message: String,
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+    email: String,
+}
+
+#[derive(Serialize)]
+struct UserResponse {
+    id: u32,
+    username: String,
 }
 
 pub async fn run() {
@@ -15,7 +30,8 @@ pub async fn run() {
     // 1. Define our routes
     let app = Router::new()
         .route("/", get(root))
-        .route("/status", get(get_status));
+        .route("/status", get(get_status))
+        .route("/users", post(create_user));
 
     // 2. Start the server
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -36,5 +52,18 @@ async fn get_status() -> Json<StatusResponse> {
     Json(StatusResponse {
         status: String::from("success"),
         message: String::from("The server is healthy and using Axum + Serde!"),
+    })
+}
+
+async fn create_user(
+    // Axum extractor: This automatically deserializes the JSON body!
+    Json(payload): Json<CreateUser>,
+) -> Json<UserResponse> {
+    println!("Received request to create user: {}", payload.username);
+
+    // In a real app, you'd save this to a database
+    Json(UserResponse {
+        id: 1337,
+        username: payload.username,
     })
 }
